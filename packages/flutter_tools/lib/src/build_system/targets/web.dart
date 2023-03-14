c072d3a4525e95547375b44b2e100e69d88bb04f
101509@nd <hooooc@gmail.com>
2023-03-14 17:53:27 +0800

    添加黑板的service worker

--- packages/flutter_tools/lib/src/build_system/targets/web.dart
+++ packages/flutter_tools/lib/src/build_system/targets/web.dart
@@ -18,6 +18,7 @@ import '../../dart/package_map.dart';
 import '../../flutter_plugins.dart';
 import '../../globals.dart' as globals;
 import '../../project.dart';
+import '../../web/file_generators/blackboard_service_worker_js.dart';
 import '../../web/file_generators/flutter_js.dart' as flutter_js;
 import '../../web/file_generators/flutter_service_worker_js.dart';
 import '../../web/file_generators/main_dart.dart' as main_dart;
@@ -524,6 +525,44 @@ class WebServiceWorker extends Target {
           'assets/AssetManifest.json',
         if (urlToHash.containsKey('assets/FontManifest.json'))
           'assets/FontManifest.json',
+      ],
+      serviceWorkerStrategy: serviceWorkerStrategy,
+    );
+    serviceWorkerFile
+      .writeAsStringSync(serviceWorker);
+    final DepfileService depfileService = DepfileService(
+      fileSystem: globals.fs,
+      logger: globals.logger,
+    );
+    depfileService.writeToFile(
+      depfile,
+      environment.buildDir.childFile('service_worker.d'),
+    );
+
+    /// chenshitao 20220814
+    buildBlackBoardServiceWorker(
+      environment: environment,
+      contents: contents,
+      urlToHash: urlToHash,
+    );
+  }
+    /// chenshitao 20220814
+  void buildBlackBoardServiceWorker({
+    required Environment environment,
+    required List<File> contents,
+    required Map<String, String> urlToHash,
+  }) {
+    final File serviceWorkerFile = environment.outputDir
+      .childFile('blackboard_service_worker.js');
+    final Depfile depfile = Depfile(contents, <File>[serviceWorkerFile]);
+    final ServiceWorkerStrategy serviceWorkerStrategy = _serviceWorkerStrategyFromString(
+      environment.defines[kServiceWorkerStrategy],
+    );
+    final String serviceWorker = generateBlackboardServiceWorker(
+      urlToHash,
+      <String>[
+        '/',
+        'index.html',
       ],
       serviceWorkerStrategy: serviceWorkerStrategy,
     );
